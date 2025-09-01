4da877668f07f4eb53ab7ab315b2712ffb24f902
ErinaHoshikawa@bellini <hoshikawa@interplan-inc.com>
2025-09-01 17:01:03 +0900

    0901 ページスクロールのプログレスバー完成

--- 02javascript/pageScroll/js/script.js
+++ 02javascript/pageScroll/js/script.js
@@ -2,7 +2,7 @@ const getScollPercent = () => {
 
   //スクロール量を取得
   const scrolled = window.scrollY;
-  console.log(`${scrolled} スクロールされました`);
+  // console.log(`${scrolled} スクロールされました`);
 
   // ページ全体の高さ
   const pageHeight = document.documentElement.scrollHeight;
@@ -10,7 +10,17 @@ const getScollPercent = () => {
   // 表示領域の高さ
   const viewHeight = document.documentElement.clientHeight;
 
-  console.log(`ページの高さ：${pageHeight}、表示領域の高さ：${viewHeight}`);
+  //取得した値をコンソールで確認用↓ 
+  //console.log(`ページの高さ：${pageHeight}、表示領域の高さ：${viewHeight}`);
 
-}
+  // スクロール量を%で＝スクロール量 ÷（ページ全体の高さ-表示画面の高さ）× 100
+  const percentage = scrolled / (pageHeight - viewHeight) * 100;
+  // console.log(percentage);
+
+  //プログレスバーに幅をwidthで設定
+  document.querySelector('#bar').style.width = `${percentage}%`;
+
+};
+
+//作った関数 getScollPercent は、画面をスクロールするたびに実行
 window.addEventListener('scroll', getScollPercent);
