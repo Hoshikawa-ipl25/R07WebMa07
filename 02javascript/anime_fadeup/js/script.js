8f3a0d870fd47d04f065f53dc7de4b164d1d7ce1
ErinaHoshikawa@bellini <hoshikawa@interplan-inc.com>
2025-09-03 15:57:33 +0900

    jsアニメ 浮き上がる見出しh1 途中まで

---
+++ 02javascript/anime_fadeup/js/script.js
@@ -0,0 +1,15 @@
+// 見出しをアニメーションさせる（制作途中）
+const heading = document.querySelector('#heading');
+
+const keyframes = {
+  opacity: [0, 1],
+  translate: ['0 50px', 0],
+}
+
+const options = {
+  //アニメーションの動きの詳細をここに記述
+};
+
+//アニメーションをするためのメソッド
+// 動かす要素.animate('動かす内容（キーフレームの設定）','動きの詳細やタイミングの設定')
+heading.animate(keyframes, 2000);
