adb1db5228a9ba6a3c9ef61d95908fe9b976b45d
ErinaHoshikawa@bellini <hoshikawa@interplan-inc.com>
2025-09-07 01:20:40 +0900

    複数画像が順にピントがあっていく効果

--- 02javascript/anime_gallary_fadeup/js/script.js
+++ 02javascript/anime_gallary_fadeup/js/script.js
@@ -1,14 +1,17 @@
 const items = document.querySelectorAll('.img-item');
 console.log(items);
 
-
+//様々な効果、初期値はCSSファイルの方も設定しつつ、ためしてみましょう
 for (let i = 0; i < items.length; i++) {
   const keyframes = {
-    opacity: [0, 1],
+    //効果追加
+    // rotate: ['-15deg', 0], // 傾いた状態から、まっすぐになる
+    // scale: [1.1, 1],       // 少し拡大した状態から、デフォルトの大きさに
+    filter: ['blur(20px)', 'blur(0)'],
   };
 
   const options = {
-    duration: 600,
+    duration: 2000,
     fill: 'forwards',
     delay: i * 300,
   };
